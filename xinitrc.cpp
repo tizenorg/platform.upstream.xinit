9259740a2d69c6c2fc1786508dbd08ae2cfef101
Jeremy Huddleston <jeremy@tifa.local>
2008-02-10 18:49:06 -0800

    Apple: Support spaces in $HOME for font path, and remove duplicate entry

--- xinitrc.cpp
+++ xinitrc.cpp
@@ -92,9 +92,8 @@ XCOMM This is the fallback case if nothing else is executed above
 if [ -x /usr/X11/bin/xset ] ; then
 	fontpath="/usr/X11/lib/X11/fonts/misc/,/usr/X11/lib/X11/fonts/TTF/,/usr/X11/lib/X11/fonts/OTF,/usr/X11/lib/X11/fonts/Type1/,/usr/X11/lib/X11/fonts/75dpi:unscaled/,/usr/X11/lib/X11/fonts/100dpi/:unscaled,/usr/X11/lib/X11/fonts/75dpi:unscaled/,/usr/X11/lib/X11/fonts/100dpi/:unscaled"
 
-	[ -e $HOME/.fonts/fonts.dir ] && fontpath="$fontpath,$HOME/.fonts"
-	[ -e $HOME/Library/Fonts/fonts.dir ] && fontpath="$fontpath,$HOME/Library/Fonts"
-	[ -e /Library/Fonts/fonts.dir ] && fontpath="$fontpath,/Library/Fonts"
+	[ -e "$HOME"/.fonts/fonts.dir ] && fontpath="$fontpath,$HOME/.fonts"
+	[ -e "$HOME"/Library/Fonts/fonts.dir ] && fontpath="$fontpath,$HOME/Library/Fonts"
 	[ -e /Library/Fonts/fonts.dir ] && fontpath="$fontpath,/Library/Fonts"
 	[ -e /System/Library/Fonts/fonts.dir ] && fontpath="$fontpath,/System/Library/Fonts"
 
